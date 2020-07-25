58eebcd5145fbcc6a3d3f6a3c4fae291569fef44
Ruben Villar <ruben@rubenvillar.net>
2020-07-25 23:17:06 +0200

    added images

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,14 @@
 module.exports = (eleventyConfig) => {
+    eleventyConfig.addLayoutAlias('default', 'layouts/default.hbs');
 
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.hbs');
-
-  return {
-    dir: {
-      input: "src/",
-      includes: "_includes"
-    },
-    templateFormats: ['hbs'],
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "hbs",
-    dataTemplateEngine: "hbs",
-  }
-}
+    return {
+        dir: {
+            input: 'src/',
+            includes: '_includes',
+        },
+        templateFormats: ['hbs', 'png', 'jpg', 'jpeg'],
+        markdownTemplateEngine: 'liquid',
+        htmlTemplateEngine: 'hbs',
+        dataTemplateEngine: 'hbs',
+    };
+};
